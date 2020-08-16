ef66d0c43f94a5727ead9351a9626d4cdd64131e
Martin Grigorov <martin.holy.grigorov@gmail.com>
2020-08-16 18:45:11 +0300

    set up fallback route in laravel

--- web/smart-home-app/routes/web.php
+++ web/smart-home-app/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any?}', function () {
     return view('welcome');
-});
+})->where('any', '^(?!api\/)[\/\w\.\,-]*'); // Mathc any route which does not start with 'api/'
