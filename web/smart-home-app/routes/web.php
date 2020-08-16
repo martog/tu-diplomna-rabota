65b0fb3e46ea994e835945090bb992a65c0ea7af
Martin Grigorov <martin.holy.grigorov@gmail.com>
2020-08-16 20:03:17 +0300

    vue routing

--- web/smart-home-app/routes/web.php
+++ web/smart-home-app/routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any?}', function () {
     return view('welcome');
-})->where('any', '^(?!api\/)[\/\w\.\,-]*'); // Mathc any route which does not start with 'api/'
+})->where('any', '^(?!api\/)[\/\w\.\,-]*'); // Match any route which does not start with 'api/'
