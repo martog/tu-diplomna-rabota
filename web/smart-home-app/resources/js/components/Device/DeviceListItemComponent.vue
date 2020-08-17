4d028508a49c5ebd18cfea49e83f1fce4ad3ad88
Martin Grigorov <martin.holy.grigorov@gmail.com>
2020-08-17 15:19:07 +0300

    design

--- web/smart-home-app/resources/js/components/Device/DeviceListItemComponent.vue
+++ web/smart-home-app/resources/js/components/Device/DeviceListItemComponent.vue
@@ -1,28 +1,36 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-xl-2">
                 <h4>{{ deviceName }}</h4>
             </div>
-            <div class="col custom-control custom-switch">
+            <div class="col-xl-10 custom-control custom-switch">
+                <!-- <div
+                    v-if="deviceStatusLoading"
+                    class="spinner-border text-primary"
+                    role="status"
+                > -->
+                <span class="sr-only">Loading...</span>
+                <!-- </div> -->
+                <!-- <div v-else> -->
                 <input
                     type="checkbox"
                     class="custom-control-input"
-                    :id="'device' + deviceId"
                     v-model="deviceActive"
-                    @change="onDeviceStatusChange()"
+                    :id="'device' + deviceId"
+                    :disabled="deviceStatusLoading"
+                    @click="onDeviceStatusChange()"
                 />
-                <label class="custom-control-label" :for="'device' + deviceId"
-                    >Status</label
-                >
+                <label
+                    class="custom-control-label"
+                    :for="'device' + deviceId"
+                ></label>
+                <!-- </div> -->
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-xl-2">
                 <p>{{ controllerName }}</p>
-            </div>
-            <div class="col-lg-6">
-                <p>{{ getDeviceStatus() }}</p>
             </div>
         </div>
     </div>
@@ -38,7 +46,8 @@ export default {
     },
     data() {
         return {
-            deviceActive: this.isDeviceActive()
+            deviceActive: this.isDeviceActive(),
+            deviceStatusLoading: false
         };
     },
     mounted() {},
@@ -50,10 +59,17 @@ export default {
             return this.deviceActive ? "On" : "Off";
         },
         onDeviceStatusChange() {
-            console.log(this.deviceActive);
+            this.deviceStatusLoading = true;
+            setTimeout(() => {
+                this.deviceStatusLoading = false;
+            }, 2000);
         }
     }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input {
+    text-align: left;
+}
+</style>
